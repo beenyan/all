202c3727602d6b34fb54ad7ead0f20074e9693c8
sdf <sdf>
2019-06-22 10:22:49 +0800

    AS

--- 2016/02_Module_C/js/function.js
+++ 2016/02_Module_C/js/function.js
@@ -1,3 +1,92 @@
 function rand(nn,mm){
   return parseInt(Math.random() * (mm - nn + 1) + nn)
-}
+};
+function max(nn,mm){
+  if (nn > mm) return nn;
+  else return mm;
+};
+function outy(nn){
+  let y = 0;
+  for (let i = 0 ; i < nn.length ; i++){
+    for (let j = 0 ; j < nn[i].length ; j++){
+      if (nn[i][j] != 0){
+        y++
+        break;
+      };
+    };
+  };
+  return y;
+};
+function outx(nn){
+  let x = {};
+  let maxx = 0;
+  for (let i = 0 ; i < nn.length ; i++){
+    x[i] = 0;
+    for (let j = 0 ; j < nn[i].length ; j++){
+      if (nn[i][j] != 0){
+        x[i]++;
+      };
+    };
+  };
+  for (let i = 0 ; i < Object.keys(x).length ; i++){
+    maxx = max(maxx,x[i]);
+  }
+  return maxx;
+};
+function leftx(nn){
+  let lx = 0;
+  for (let i = 0 ; i < nn.length ; i++){
+    if (nn[i][0] == 0){
+      lx++;
+    }
+  };
+  if (lx == nn.length) return 1
+  else return 0;
+};
+function havetop(x,y,arr){
+  let havey = 0;
+  while (y--){
+    if (arr[y][x] != 0){
+      havey ++;
+      break;
+    }
+  };
+  return havey;
+};
+function boom(nn,x,y,arr){
+  let tf = 0;
+  for (let i = 0 ; i < nn.length ; i++){
+    for (let j = 0 ; j < nn[i].length ; j++){
+      if (arr[y + i][x + j] > 0 && nn[i][j] > 0){
+        tf++;
+        break;
+      }
+    };
+  };
+  return tf;
+};
+function deleblock(arr){
+  let waitarr = arr;
+  let line = 18;
+  let linck = 0;
+  while (line--){
+    for (let i = 0 ; i < 10 ; i++){
+      if (arr[line][i] != 0){
+        linck++;
+      };
+    };
+    if (linck == 10){//整排都是
+      for (let i = 0 ; i < 10 ; i++){
+        waitarr[line][i] = 0;
+      };
+      line++;
+      let templine = line;
+      while (templine > 1){
+        waitarr[templine] = waitarr[templine-1];
+        templine--;
+      };
+    };
+    linck = 0;
+  };
+  return waitarr;
+};
--- 2016/02_Module_C/js/main.js
+++ 2016/02_Module_C/js/main.js
@@ -2,7 +2,57 @@ $(function(){
   $("html").keydown(function(e){
     if (e.keyCode == 40){
       down();
-    };
+    }
+    else if (e.keyCode == 37){//左
+      for (let i = player.y ; i < player.block.length + player.y ; i++){//左邊有東西
+        for (let j = player.x - 1 ; j < player.block[i - player.y].length + player.x - 1 ; j++){
+          if (player.block[i - player.y][j - player.x + 1] != 0){
+            if(arr[i][j] != 0){
+              return false;
+            }
+          };
+        };
+      };
+      if (player.x == 0) {
+        let left = 0;
+        for (let i = 0 ; i < player.block.length ; i++){
+          left += player.block[i][0];
+        };
+        if (left) return false;
+      };
+      player.x--;
+    }
+    else if (e.keyCode == 38){//上
+      while (player.x < 0) player.x++;//左邊超過邊界
+      while (outx(nowblock.block[(nowblock.style + 1) % Object.keys(nowblock.block).length]) + player.x > 10) player.x--;//右邊超過邊界
+      while (outy(nowblock.block[(nowblock.style + 1) % Object.keys(nowblock.block).length]) + player.y > 18) player.y--;//下面超過邊界
+      while (havetop(//底部有東西
+        leftx(nowblock.block[(nowblock.style + 1) % Object.keys(nowblock.block).length]) + player.x,
+        outy(nowblock.block[(nowblock.style + 1) % Object.keys(nowblock.block).length]) + player.y,
+        arr
+      )) player.y--;
+      if (boom(
+        nowblock.block[(nowblock.style + 1) % Object.keys(nowblock.block).length],
+        leftx(nowblock.block[(nowblock.style + 1) % Object.keys(nowblock.block).length]) + player.x,
+        player.y,
+        arr
+      )) return false;
+      nowblock.style = (nowblock.style + 1) % Object.keys(nowblock.block).length;
+    }
+    else if (e.keyCode == 39){//右
+      let maxx = outx(player.block);
+      for (let i = player.y ; i < player.block.length + player.y ; i++){//左邊有東西
+        for (let j = player.x + 1 ; j < player.block[i - player.y].length + player.x + 1 ; j++){
+          if (player.block[i - player.y][j - player.x - 1] != 0){
+            if(arr[i][j] != 0){
+              return false;
+            }
+          };
+        };
+      };
+      if (player.x + maxx == 10) return false;
+      player.x++;
+    }
   });
   let canvas = document.createElement('canvas');
   let ctx = canvas.getContext('2d');
@@ -15,7 +65,6 @@ $(function(){
   ctx1.scale(zoom,zoom);
   $("#background").append(canvas);
   $("#background").append(canvas1);
-  ctx.fillRect(0,0,10,18);
   //初始陣列
   let arr = [];
   for (let i = 0 ; i < canvas.height / zoom ; i++){
@@ -25,6 +74,33 @@ $(function(){
     };
     arr.push(list);
   };
+  function cgcolor(n){
+    if (n == 0){
+      return "rgba(0,0,0,1)";
+    }
+    else if (n == 1){
+      return "rgba(160,0,240,1)"
+    }
+    else if (n == 2){
+      return "rgba(255,102,0,1)"
+    }
+    else if (n == 3){
+      return "rgba(0,0,255,1)"
+    }
+    else if (n == 3){
+      return "rgba(255,0,0,1)"
+    }
+    else return "rgba(255,0,0,1)"
+  };
+  function drawbackground(){//畫背景
+    for (let i = 0 ;i < arr.length ; i++){
+      for (let j = 0 ; j < arr[i].length ; j++){
+        ctx.fillStyle = cgcolor(arr[i][j]);
+        ctx.fillRect(j,i,1,1);
+      };
+    };
+  };
+  drawbackground();
   //方塊
   let T = {
     0 : [
@@ -33,24 +109,34 @@ $(function(){
       [0,0,0],
     ],
     1 : [
+      [0,0,1],
+      [0,1,1],
+      [0,0,1],
+    ],
+    2 : [
+      [0,1,0],
+      [1,1,1],
+      [0,0,0],
+    ],
+    3 : [
       [1,0,0],
       [1,1,0],
       [1,0,0],
-    ],
+    ]
   };
   let I = {
     0 : [
-      [2,0,0,0],
-      [2,0,0,0],
-      [2,0,0,0],
-      [2,0,0,0],
+      [0,2,0,0],
+      [0,2,0,0],
+      [0,2,0,0],
+      [0,2,0,0],
     ],
     1 : [
-      [0,0,0,0],
       [2,2,2,2],
       [0,0,0,0],
       [0,0,0,0],
-    ],
+      [0,0,0,0],
+    ]
   };
   let L = {
     0 : [
@@ -59,10 +145,20 @@ $(function(){
       [0,3,0],
     ],
     1 : [
+      [0,0,3],
+      [3,3,3],
+      [0,0,0],
+    ],
+    2 : [
+      [3,0,0],
+      [3,0,0],
+      [3,3,0],
+    ],
+    3 : [
       [3,3,3],
       [3,0,0],
       [0,0,0],
-    ],
+    ]
   };
   let O = {
     0 : [
@@ -72,11 +168,15 @@ $(function(){
     1 : [
       [4,4],
       [4,4],
-    ],
+    ]
   };
   let allblock = [T,I,L,O];
+  let nowblock = {
+    block : allblock[rand(0,allblock.length-1)],
+    style : 0,
+  }
   let player = {
-    block : allblock[rand(0,allblock.length-1)][0],
+    block : nowblock.block[nowblock.style],
     x : parseInt(canvas.width / zoom / 2.5),
     y : 0,
   };
@@ -84,39 +184,59 @@ $(function(){
     ctx1.clearRect(0,0,canvas1.width,canvas1.height);
     let x = player.x;
     let y = player.y;
-    ctx1.fillStyle = "red";
     for (let i = 0 ; i < player.block.length ; i++){
       for (let j = 0 ; j < player.block[i].length ; j++){
         if (player.block[i][j] != 0){
+          ctx1.fillStyle = cgcolor(player.block[i][j]);
           ctx1.fillRect(x + j,y + i,1,1);
         };
       };
     };
+    player.block = nowblock.block[nowblock.style];
   };
   drawblock();
   let dwonspeed = 500;
-  function down(){//方塊下降
-    let y = 0;
-    for (let i = 0 ; i < player.block.length ; i++){
-      for (let j = 0 ; j < player.block[i].length ; j++){
-        if (player.block[i][j] != 0){
-          y++;
-          break;
+  function pusharr(){//填陣列 畫方塊 刷新螢幕 清除畫布 刷新方塊
+    for (let i = player.y ; i < player.block.length + player.y ; i++){//填陣列
+      for (let j = player.x ; j < player.block[i - player.y].length + player.x ; j++){
+        if (player.block[i - player.y][j - player.x] != 0){
+          arr[i][j] = player.block[i - player.y][j - player.x]; 
         };
       };
     };
+    arr = deleblock(arr);
+    drawbackground();
+    ctx1.clearRect(0,0,canvas1.width,canvas1.height); //清除畫布
+    nowblock = {
+      block : allblock[rand(0,allblock.length-1)],
+      style : 0,
+    }
+    player = {
+      block : nowblock.block[nowblock.style],
+      x : parseInt(canvas.width / zoom / 2.5),
+      y : 0,
+    };
+  };
+  function down(){//方塊下降
+    let y = outy(player.block);
     if ((player.y + 1) + y > 18){//超過螢幕
-      for (let i = player.y ; i < player.block.length + player.y ; i++){
-        for (let j = player.x ; j < player.block[i - player.y].length + player.x ; j++){
-          if (player.block[i - player.y][j - player.x] != 0){
-            arr[i][j] = player.block[i - player.y][j - player.x]; //填陣列
+      pusharr();
+      return false;
+    };
+    for (let i = (player.y + 1) ; i < player.block.length + (player.y + 1) ; i++){//壓到下面的
+      for (let j = player.x ; j < player.block[i - player.y - 1].length + player.x ; j++){
+        if (player.block[i - player.y - 1][j - player.x] != 0){
+          if(arr[i][j] != 0){
+            pusharr();
+            return false;
           };
         };
       };
-      return false;
     };
     player.y++;
-    drawblock();
   };
   setInterval(down,dwonspeed);
+  setInterval(function(){
+    drawblock();
+  },1000/60);
 });
