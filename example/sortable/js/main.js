0762b5f44ca09c35de1f5155578b5bb33cc9a8f4
sdf <sdf>
2019-07-28 08:11:49 +0800

    asdasd

--- example/sortable/js/main.js
+++ example/sortable/js/main.js
@@ -1,2 +1,12 @@
 //<script>
+  $(".sort").sortable({
+    connectWith : ".sort",
+  });
+  function aa(){
+    let arr = "";
+    for (let i = 0 ; i < $(".sort:eq(1) .img").length ; i++){
+      arr += $(`.sort:eq(1) .img:eq(${i})`).text();
+    };
+    alert(arr);
+  }
 //</script>
